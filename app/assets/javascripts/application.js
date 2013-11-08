f1438e37a4b72303f1234f2f3e68ebb188b900f5
Marks Calderón <artmar89@gmail.com>
2013-11-08 01:13:47 -0500

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require html5-trunk.js
+//= require wysiwyg/wysihtml5-0.3.0
+//= require jquery.min
+//= require bootstrap
+//= require jquery-ui-1.8.23.custom.min
+//= require flot/jquery.flot
+//= require	flot/jquery.flot.resize.min
+//= require flot/jquery.flot.tooltip
+//= require flot/jquery.flot.pie
+//= require flot/jquery.flot.selection
+//= require jquery.easy-pie-chart
+//= require tiny-scrollbar
+//= require jquery.sparkline
+//= require rating/jquery.raty
+//= require custom-index
+//= require custom.js
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lean::Application.config.secret_key_base = 'd0956b63f1d37f7f41af0c22c4bc5123dd55a62e58864432ae1cce85fe2c893700ff9fe5bfcfb455445653b2ad303f15a6f6e7ad77cab37ec2edbc392c12eaab'
