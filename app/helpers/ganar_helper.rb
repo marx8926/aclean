fb49bacef47452a97470aa9e184da03777d02796
marx8926 <artmar89@gmail.com>
2013-11-26 09:01:05 -0500

    registrar gaanr sin transact

--- app/helpers/ganar_helper.rb
+++ app/helpers/ganar_helper.rb
@@ -1,2 +1,5 @@
 module GanarHelper
+	def lugar_select
+		select("", "lugar", Lugar.all.collect {|p| [ p.var_lugar_descripcion, p.int_lugar_id ] }, {}, { class: "form-control"})
+	end
 end
--- app/models/persona.rb
+++ app/models/persona.rb
@@ -2,8 +2,10 @@ class Persona < ActiveRecord::Base
 	belongs_to :iglesia
 	has_many :telefonos
 	belongs_to :persona
+	belongs_to :lugar
 	has_many :nivel_crecimientos
 	has_many :diezmos
 	has_many :asistencias
 	has_many :direccions
+	has_many :peticions
 end
