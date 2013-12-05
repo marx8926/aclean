4855163fec9e25d7015df618763966147fde5ee0
lavaldi <clindy.26@gmail.com>
2013-12-05 10:12:56 -0500

    cambios vista usuarios

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-  $(".datepicker").datepicker({
-    format:"dd/mm/yyyy"
-  });
+	$(".wizard").bwizard({nextBtnText: "Siguiente &rarr;", backBtnText: "&larr; Anterior"})
+	$(".datepicker").datepicker({
+		format:"dd/mm/yyyy"
+	});
 });
